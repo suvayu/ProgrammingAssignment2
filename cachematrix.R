16f1be339a944b5698c40b2186e8cec6662b6e36
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2014-06-20 20:39:19 +0200

    Update with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming assignment 2
+
 
 ## return a "matrix" with inverse caching using a list
 
 makeCacheMatrix <- function(mat = matrix()) {
+  ## initialise inverse to NULL
   inv <- NULL
-  set <- function(val) {
-    mat <<- val
+
+  ## setter function
+  set <- function(value) {
+    mat <<- value
+    ## reset inverse, since not valid any more
     inv <- NULL
   }
+  ## getter function
   get <- function() mat
+
+  ## setter function for inverse
   setinv <- function(inverse) inv <<- inverse
+  ## getter function for inverse
   getinv <- function() inv
+
+  ## return "matrix" with caching
   list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
@@ -19,11 +29,15 @@ makeCacheMatrix <- function(mat = matrix()) {
 ## retrieve cached inverse or calculate and update cache and return
 
 cacheSolve <- function(mat, ...) {
+  ## get inverse
   inv <- mat$getinv()
+  ## return cache if inverse is valid
   if(!is.null(inv)) {
     message("Using cached data")
     return(inv)
   }
+
+  ## calculate inverse otherwise
   data <- mat$get()
   inv <- solve(data, ...)
   mat$setinv(inv)
